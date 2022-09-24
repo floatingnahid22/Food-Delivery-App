473d51ea3af047d44ad59118323da5bf025421ec
floatingnahid22 <nahid.uap.github@gmail.com>
2022-09-24 18:34:55 +0600

    home container part-2

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { deafult as CreateContainer } from "./CreateContainer";
 export { deafult as Home } from "./Header";
+export { deafult as HomeContainer } from "./HomeContainer";
 export { deafult as MainContainer } from "./MainContainer";
