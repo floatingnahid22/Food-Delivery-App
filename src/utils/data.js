d53f9eba05fd50afd6721a55888409c342c8331a
floatingnahid22 <nahid.uap.github@gmail.com>
2022-09-24 19:10:01 +0600

    home container part-3

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import C3 from "../img/c3.png";
+import F1 from "../img/f1.png";
+import Fi1 from "../img/fi1.png";
+import I1 from "../img/i1.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Icecream",
+    decp: "Chocolate & vanilla",
+    price: "499",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Strawberries",
+    decp: "Fresh Strawberries",
+    price: "999",
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Chicken Kebab",
+    decp: "Mixed Kebab Plate",
+    price: "799",
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Fish Kebab",
+    decp: "Mixed Fish Kebab",
+    price: "599",
+    imageSrc: Fi1,
+  },
+];
