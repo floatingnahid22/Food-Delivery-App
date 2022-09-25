42468884fd5ea736bcb970259acbc38996f5a8fc
floatingnahid22 <nahid.uap.github@gmail.com>
2022-09-25 13:25:03 +0600

    fruits and cart section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,29 @@
 import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { MdAdd, MdLogout, MdShoppingBasket } from "react-icons/md";
-import { Link } from "react-router-dom";
-import Avatar from "../img/avatar.png";
-import Logo from "../img/logo.png";
 
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase.config";
+
+import { Link } from "react-router-dom";
 import { actionType } from "../context/reducer";
 import { useStateValue } from "../context/StateProvider";
-import { app } from "../firebase.config";
+import Avatar from "../img/avatar.png";
+import Logo from "../img/logo.png";
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
-  const [{ user }, dispatch] = useStateValue();
+  const [{ user, cartShow, cartItems }, dispatch] = useStateValue();
+
   const [isMenu, setIsMenu] = useState(false);
 
   const login = async () => {
     if (!user) {
       const {
-        user: { providerData },
+        // eslint-disable-next-line
+        user: { refreshToken, providerData },
       } = await signInWithPopup(firebaseAuth, provider);
       dispatch({
         type: actionType.SET_USER,
@@ -42,14 +45,22 @@ const Header = () => {
     });
   };
 
+  const showCart = () => {
+    dispatch({
+      type: actionType.SET_CART_SHOW,
+      cartShow: !cartShow,
+    });
+  };
+
   return (
-    <header className="fixed z-50 w-screen  p-3 px-4 md:p-6 md:px-16 bg-primary">
-      {/* For desktop & tablet */}
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
+      {/* desktop & tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <Link to={"/"} className="flex items gap-2">
+        <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
-          <p className="text-headingColor text-xl font-bold">Meal-Route</p>
+          <p className="text-headingColor text-xl font-bold"> Meal-Route</p>
         </Link>
+
         <div className="flex items-center gap-8">
           <motion.ul
             initial={{ opacity: 0, x: 200 }}
@@ -70,19 +81,27 @@ const Header = () => {
               Service
             </li>
           </motion.ul>
-          <div className="relative flex items-center justify-center">
-            <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
-            <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
-              <p className="text-xs text-white font-semibold">2</p>
-            </div>
+
+          <div
+            className="relative flex items-center justify-center"
+            onClick={showCart}
+          >
+            <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+            {cartItems && cartItems.length > 0 && (
+              <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                <p className="text-xs text-white font-semibold">
+                  {cartItems.length}
+                </p>
+              </div>
+            )}
           </div>
 
           <div className="relative">
             <motion.img
               whileTap={{ scale: 0.6 }}
               src={user ? user.photoURL : Avatar}
-              className="w-7 min-w-[28px] h-7 min-h-[28px] drop-shadow-xl cursor-pointer rounded-full"
-              alt="userProfile"
+              className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+              alt="userprofile"
               onClick={login}
             />
             {isMenu && (
@@ -114,26 +133,34 @@ const Header = () => {
           </div>
         </div>
       </div>
-      {/* For mobile */}
-      <div className="flex items-center justify-between md:hidden w-full h-full">
-        <div className="relative flex items-center justify-center">
-          <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
-          <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
-            <p className="text-xs text-white font-semibold">2</p>
-          </div>
+
+      {/* mobile */}
+      <div className="flex items-center justify-between md:hidden w-full h-full ">
+        <div
+          className="relative flex items-center justify-center"
+          onClick={showCart}
+        >
+          <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+          {cartItems && cartItems.length > 0 && (
+            <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">
+                {cartItems.length}
+              </p>
+            </div>
+          )}
         </div>
 
-        <Link to={"/"} className="flex items gap-2">
+        <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
-          <p className="text-headingColor text-xl font-bold">Meal-Route</p>
+          <p className="text-headingColor text-xl font-bold"> Meal-Route</p>
         </Link>
 
         <div className="relative">
           <motion.img
             whileTap={{ scale: 0.6 }}
             src={user ? user.photoURL : Avatar}
-            className="w-7 min-w-[28px] h-7 min-h-[28px] drop-shadow-xl cursor-pointer rounded-full"
-            alt="userProfile"
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+            alt="userprofile"
             onClick={login}
           />
           {isMenu && (
@@ -145,10 +172,7 @@ const Header = () => {
             >
               {user && user.email === "nahidhossain1184@gmail.com" && (
                 <Link to={"/createItem"}>
-                  <p
-                    className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
-                    onClick={() => setIsMenu(false)}
-                  >
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
                     New Item <MdAdd />
                   </p>
                 </Link>
@@ -182,7 +206,7 @@ const Header = () => {
               </ul>
 
               <p
-                className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                className="m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base"
                 onClick={logout}
               >
                 Logout <MdLogout />
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
-export { deafult as CreateContainer } from "./CreateContainer";
-export { deafult as Home } from "./Header";
-export { deafult as HomeContainer } from "./HomeContainer";
-export { deafult as Loader } from "./Loader";
-export { deafult as MainContainer } from "./MainContainer";
+export { default as CartContainer } from "./CartContainer";
+export { default as CartItem } from "./CartItem";
+export { default as CreateContainer } from "./CreateContainer";
+export { default as Header } from "./Header";
+export { default as HomeContainer } from "./HomeContainer";
+export { default as Loader } from "./Loader";
+export { default as MainContainer } from "./MainContainer";
+export { default as MenuContainer } from "./MenuContainer";
+export { default as RowContainer } from "./RowContainer";
